4da384dadfd7cc6e124649ca87d398a117d60457
Peter Sansom <peetr.san@gmail.com>
2019-06-10 16:03:58 +0930

    Website 1 commit

---
+++ data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-start",
+      "name": "start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3149,
+      "initialViewParameters": {
+        "yaw": 0.012361336582078053,
+        "pitch": 0.005216816985422312,
+        "fov": 1.4182622546764196
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.002786089574097872,
+          "pitch": 0.20779123462403426,
+          "rotation": 0,
+          "target": "3-commerical"
+        },
+        {
+          "yaw": 0.6113931486472346,
+          "pitch": 0.2299813722584716,
+          "rotation": 0,
+          "target": "2-real-estate"
+        },
+        {
+          "yaw": 1.2571636178819041,
+          "pitch": 0.28073552890149855,
+          "rotation": 0,
+          "target": "1-parks"
+        },
+        {
+          "yaw": -0.6129988222683451,
+          "pitch": 0.21717216516077364,
+          "rotation": 0,
+          "target": "4-outdoor"
+        },
+        {
+          "yaw": -1.265042910793115,
+          "pitch": 0.2473754177244647,
+          "rotation": 0,
+          "target": "5-other"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parks",
+      "name": "Parks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3149,
+      "initialViewParameters": {
+        "yaw": -0.06776933305124189,
+        "pitch": 0.1367531256453045,
+        "fov": 1.4182622546764196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17949476632663774,
+          "pitch": 0.7807457347454445,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-real-estate",
+      "name": "Real Estate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3142,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4182622546764196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0666213068316317,
+          "pitch": 0.5686679572208426,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-commerical",
+      "name": "Commerical",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3138.5,
+      "initialViewParameters": {
+        "yaw": -0.04679248860507812,
+        "pitch": 0.008822782299695575,
+        "fov": 1.4182622546764196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06927777029212301,
+          "pitch": 0.42280839982525364,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-outdoor",
+      "name": "Outdoor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3149,
+      "initialViewParameters": {
+        "yaw": 0.4082295538563052,
+        "pitch": -0.020697336362731633,
+        "fov": 1.4182622546764196
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.35517574668482155,
+          "pitch": 0.4495303692426056,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-other",
+      "name": "Other",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3151,
+      "initialViewParameters": {
+        "yaw": -0.9649062182057904,
+        "pitch": 0.2713005557156958,
+        "fov": 1.4182622546764196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9615779860771738,
+          "pitch": 0.814028762740179,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Peter Sansom Panoramic Photographer",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
